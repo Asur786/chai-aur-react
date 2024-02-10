498708a60e7fddf59b91214e4865339a3c792893
1stGithubProject <abhi.kmr8210@gmail.com>
2024-02-10 19:02:59 +0530

    basicmap

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  const actresses=[{
+    id:1,
+    userName:"Katrina Kaif",
+    imgLink:"https://th.bing.com/th/id/OIP.dcXFLuIM8Lee8j_Yc1kejwHaKV?rs=1&pid=ImgDetMain"
+  },
+  {
+    id:2,
+    userName: "Shraddha Kapoor",
+    imgLink: "https://famousbiography.in/wp-content/uploads/2020/12/Shraddha-Kapoor-Biography.jpg" 
+  }
+]
+
+const listItems=actresses.map(actress=>
+  <li key={actress.id}>
+    <Card name={actress.userName} image={actress.imgLink} />
+  </li>
+)
 
   return (
     <>
-      <div className="card">
+      {/* <div className="card">
         <h1>Chai aur react</h1>
         <h2>Your count is now: {count}</h2>
         <button onClick={() =>{
@@ -22,9 +40,15 @@ function App() {
           } 
         }}>
           Decrease Value {count}
-        </button>
+        </button> */}
 
-      </div>
+      {/* </div> */}
+
+      <h1 className="text-black rounded-xl bg-white p-2">Tailwind Test</h1>
+      
+      {listItems}
+
+      {/** <Card/>  Now problem is what i am getting another card with same character photo and her name. Therfore props comes to exist here*/}
     </>
   )
 }
